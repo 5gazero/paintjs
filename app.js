d8805329d4c18eba6ddeeb4cb6fe86258fba0e17
5gazero <kayo9410@gmail.com>
2022-01-19 03:10:16 +0900

    [feat]: Filling

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
 
 // pixel modifier
-canvas.width = 700;
-canvas.height = 700;
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
 
-ctx.strokeStyle = "#2c2c2c";
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 const stopPainting = () => {
   painting = false;
@@ -35,24 +41,46 @@ const onMouseMove = (event) => {
 const handleColorClick = (event) => {
   const color = event.target.style.backgroundColor;
   ctx.strokeStyle = color;
-}
+  ctx.fillStyle = color;
+};
 
 const handleRangeChange = (event) => {
   const size = event.target.value;
   ctx.lineWidth = size;
-}
+};
+
+const handleModeClick = () => {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "Paint";
+  }
+};
+
+const handleCanvasClick = () => {
+  if (filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
+};
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
 }
 
 if (range) {
   range.addEventListener("input", handleRangeChange);
 }
 
-Array.from(colors).forEach(color =>
-  color.addEventListener("click", handleColorClick)
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
+}
+
+Array.from(colors).forEach((color) =>
+  color.addEventListener("click", handleColorClick),
 );
